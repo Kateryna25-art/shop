65e44c3eea00ef86fd31fe7d930cf8201be49530
Kateryna25-art <kocherova.ekaterina25@gmail.com>
2026-04-25 15:52:00 +0300

    added adaptive header+footer

--- app/js/main.js
+++ app/js/main.js
@@ -28,7 +28,7 @@ const swiperReviews = new Swiper('.reviews__slider', {
 });
 
 const rangeSlider = document.querySelector('.range__slider');
-if(rangeSlider) {
+if (rangeSlider) {
   const inputMin = document.querySelector('.range__min');
   const inputMax = document.querySelector('.range__max');
   const inputs = [inputMin, inputMax];
@@ -46,40 +46,46 @@ if(rangeSlider) {
     }
   });
   rangeSlider.noUiSlider.on('update',
-   (values, handle) => {
-    inputs[handle].value = values[handle];
-   });
-   inputs.forEach((input, index) => {
-input.addEventListener('change', () =>{
-  rangeSlider.noUiSlider.setHandle(index, input.value);
-});
-   });
- 
-}
-
-
-const modeContainer = document.querySelectorAll('.view-mode__container');
-const modeBtnGrid =  document.querySelectorAll('.view-mode__btn--grid');
-const modeBtnLine =  document.querySelectorAll('.view-mode__btn--line');
-
-
-
-
-if(modeBtnGrid.length > 0) {
-modeBtnGrid.forEach(btn =>{
-  btn.addEventListener('click', () =>{
-    console.log('контейнеров не найдено:',
-      modeContainer.length  );
-  
-    modeContainer.forEach(container => {
-      container.classList.add('view-mode__container--grid');
-      container.classList.remove('view-mode__container--line');
+    (values, handle) => {
+      inputs[handle].value = values[handle];
+    });
+  inputs.forEach((input, index) => {
+    input.addEventListener('change', () => {
+      rangeSlider.noUiSlider.setHandle(index, input.value);
     });
   });
-});
+
 }
 
-if(modeBtnLine.length > 0) {
+const headerBtn = document.querySelector('.header__btn');
+const menu = document.querySelector('.menu');
+
+headerBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active')
+});
+
+const modeContainer = document.querySelectorAll('.view-mode__container');
+const modeBtnGrid = document.querySelectorAll('.view-mode__btn--grid');
+const modeBtnLine = document.querySelectorAll('.view-mode__btn--line');
+
+
+
+
+if (modeBtnGrid.length > 0) {
+  modeBtnGrid.forEach(btn => {
+    btn.addEventListener('click', () => {
+      console.log('контейнеров не найдено:',
+        modeContainer.length);
+
+      modeContainer.forEach(container => {
+        container.classList.add('view-mode__container--grid');
+        container.classList.remove('view-mode__container--line');
+      });
+    });
+  });
+}
+
+if (modeBtnLine.length > 0) {
   modeBtnLine.forEach(btn => {
     btn.addEventListener('click', () => {
       console.log('click line');
@@ -90,11 +96,3 @@ if(modeBtnLine.length > 0) {
     });
   });
 }
-
-
-
- 
-
-
-
-
